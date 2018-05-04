f91891f7a664a03f069755192f49d042b146721e
Milad Nekooei <mi.nekooei@gmail.com>
2018-05-04 16:27:53 +0430

    Create AddEntry Component
    For the starting this Component showing only an Icon from @expo/icons

---
+++ components/AddEntry.js
@@ -0,0 +1,16 @@
+import React , { Component } from 'react'
+import { Text, View } from 'react-native'
+import { getMetricMetaInfo } from '../utils/helpers'
+
+
+export default class AddEntry extends Component  {
+
+    render() {
+        return (
+            <View>
+                {getMetricMetaInfo('bike').getIcon()}
+            </View>
+        );
+    }
+
+}
