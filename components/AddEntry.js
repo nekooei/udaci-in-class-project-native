fa58e60c6c24f7a1ac25cf9dd264a9d142f41489
Milad Nekooei <mi.nekooei@gmail.com>
2018-05-04 16:43:59 +0430

    Create increment method in AddEntry Component

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,16 +1,37 @@
-import React , { Component } from 'react'
-import { Text, View } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import React, {Component} from 'react'
+import {Text, View} from 'react-native'
+import {getMetricMetaInfo} from '../utils/helpers'
 
 
-export default class AddEntry extends Component  {
+export default class AddEntry extends Component {
 
-    render() {
-        return (
-            <View>
-                {getMetricMetaInfo('bike').getIcon()}
-            </View>
-        );
-    }
+	state = {
+		bike: 0,
+		run: 0,
+		swim: 0,
+		sleep: 0,
+		eat: 0
+	}
+
+	increment = (metric) => {
+		const {max, step } = getMetricMetaInfo(metric)
+
+		this.setState(state => {
+			const count = state[metric] + step
+
+			return {
+				...state,
+				[metric] : count > max ? max : count
+			}
+		})
+	}
+
+	render() {
+		return (
+			<View>
+				{getMetricMetaInfo('bike').getIcon()}
+			</View>
+		);
+	}
 
 }
