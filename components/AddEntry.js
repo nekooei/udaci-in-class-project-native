0a9df2ef92a36e717c681f14c27230d64527143a
Milad Nekooei <mi.nekooei@gmail.com>
2018-05-04 16:45:53 +0430

    Create decrement method

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -14,14 +14,25 @@ export default class AddEntry extends Component {
 	}
 
 	increment = (metric) => {
-		const {max, step } = getMetricMetaInfo(metric)
+		const {max, step} = getMetricMetaInfo(metric)
 
 		this.setState(state => {
 			const count = state[metric] + step
 
 			return {
 				...state,
-				[metric] : count > max ? max : count
+				[metric]: count > max ? max : count
+			}
+		})
+	}
+
+	decrement = (metric) => {
+		this.setState(state => {
+			const count = state[metric] - getMetricMetaInfo(metric).step
+
+			return {
+				...state,
+				[metric]: count < 0 ? 0 : count
 			}
 		})
 	}
