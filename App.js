1fc0feb6e4be2fb7e55083dd24777872eefe5819
Milad Nekooei <mi.nekooei@gmail.com>
2018-05-06 19:08:40 +0430

    Wrap application into provider

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import { StyleSheet,  View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import AddEntry from "./components/AddEntry";
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import reducer from './reducers'
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <AddEntry/>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<Provider store={createStore(reducer)}>
+				<View>
+					<AddEntry/>
+				</View>
+			</Provider>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
