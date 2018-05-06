ef509020f9ae4ae121bd4fbd65a3c2c52fb1c488
Milad Nekooei <mi.nekooei@gmail.com>
2018-05-06 19:37:44 +0430

    Create function for showing message in helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -246,3 +246,9 @@ export function getMetricMetaInfo(metric) {
         ? info
         : info[metric]
 }
+
+export function getDailyReminderValue() {
+    return{
+        today : "Don't forget to log your data today!",
+    }
+}
