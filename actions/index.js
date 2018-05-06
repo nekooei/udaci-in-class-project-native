71363c0850a876dcb29acc0e4bbf2e9a3514c021
Milad Nekooei <mi.nekooei@gmail.com>
2018-05-06 19:36:38 +0430

    Fix typos

--- actions/index.js
+++ actions/index.js
@@ -4,14 +4,14 @@ export const ADD_ENTRY = "ADD_ENTRY"
 
 export function receiveEntries(entries) {
 	return{
-		action: RECEIVE_ENTRRIES,
+		type: RECEIVE_ENTRRIES,
 		entries
 	}
 }
 
 export function addEntry(entry) {
 	return{
-		action: ADD_ENTRY,
+		type: ADD_ENTRY,
 		entry
 	}
-}f
+}
