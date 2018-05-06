923698203357686ee8e14bf745c491398abb7ecc
Milad Nekooei <mi.nekooei@gmail.com>
2018-05-06 18:53:18 +0430

    Export action types constant

--- actions/index.js
+++ actions/index.js
@@ -1,6 +1,6 @@
-const RECEIVE_ENTRRIES = 'RECEIVE_ENTRIES'
+export const RECEIVE_ENTRRIES = 'RECEIVE_ENTRIES'
 
-const ADD_ENTRY = "ADD_ENTRY"
+export const ADD_ENTRY = "ADD_ENTRY"
 
 export function receiveEntries(entries) {
 	return{
